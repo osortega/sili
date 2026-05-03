5a4a52feb24fb4ab071d24794ae9b85b3acf9467
Osvaldo Ortega <48293249+osortega@users.noreply.github.com>
2026-05-03 11:17:12 -0700

    Delete Sili formula from homebrew
    Remove the Sili formula from Homebrew.

--- homebrew/sili.rb
+++ homebrew/sili.rb
@@ -1,18 +1,1 @@
-class Sili < Formula
-  desc     "Codespace resource manager with auto-tunnel"
-  homepage "https://github.com/osortega/sili"
-  url      "https://github.com/osortega/sili/archive/refs/tags/v0.1.0.tar.gz"
-  sha256   "REPLACE_WITH_SHA256_FROM_CURL"
-  license  "MIT"
 
-  depends_on "gh"
-
-  def install
-    libexec.install "bin", "lib"
-    bin.install_symlink libexec/"bin/sili"
-  end
-
-  test do
-    assert_match "sili", shell_output("#{bin}/sili help")
-  end
-end
